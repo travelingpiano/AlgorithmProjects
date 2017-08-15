7a68195372523705e5edc9fac0e4ca76084ce259
travelingpiano <lihsuanlu@gmail.com>
2017-08-15 11:52:12 -0700

    finish third part

--- DynamicArray/LiHsuan_Lu/lib/ring_buffer.rb
+++ DynamicArray/LiHsuan_Lu/lib/ring_buffer.rb
@@ -7,6 +7,7 @@ class RingBuffer
     @store = Array.new(8)
     @start_idx = 0
     @length = 0
+    @capacity = 8
   end
 
   # O(1)
@@ -14,27 +15,55 @@ class RingBuffer
     if index >= length
       raise 'index out of bounds'
     end
-    @store[index]
+    @store[(@start_idx+index) % capacity]
   end
 
   # O(1)
   def []=(index, val)
+    @store[(@start_idx+index) % capacity] = val
   end
 
   # O(1)
   def pop
+    if length == 0
+      raise 'index out of bounds'
+    end
+    value = @store[(@start_idx+@length-1) % @capacity]
+    @length -= 1
+    value
   end
 
   # O(1) ammortized
   def push(val)
+    if @length == @capacity
+      resize!
+    end
+    new_idx = (@start_idx + @length) % @capacity
+    @store[new_idx] = val
+    @length += 1
+    @store
   end
 
   # O(1)
   def shift
+    if length == 0
+      raise 'index out of bounds'
+    end
+    old_idx = @start_idx
+    @start_idx = (@start_idx+1) % @capacity
+    @length -= 1
+    @store[old_idx]
   end
 
   # O(1) ammortized
   def unshift(val)
+    if length == @capacity
+      resize!
+    end
+    @start_idx = (@start_idx - 1) % @capacity
+    @store[@start_idx] = val
+    @length += 1
+    @store
   end
 
   protected
@@ -45,5 +74,12 @@ class RingBuffer
   end
 
   def resize!
+    old_store = @store.dup
+    @capacity = @capacity*2
+    @store = Array.new(@capacity)
+    old_store.each_index do |idx|
+      @store[(@start_idx+idx) % (@capacity/2)] = old_store[idx]
+    end
+    @start_idx = 0
   end
 end
