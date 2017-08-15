cedf70010711171d4516b4146df3d198f76d0fdf
travelingpiano <lihsuanlu@gmail.com>
2017-08-15 16:46:12 -0700

    finish hash

--- LRUCache/LiHsuan_Lu/lib/p02_hashing.rb
+++ LRUCache/LiHsuan_Lu/lib/p02_hashing.rb
@@ -4,11 +4,18 @@ end
 
 class Array
   def hash
+    hash_num = 0
+    self.each_with_index{|num, idx| hash_num += num*idx}
+    hash_num.hash
   end
 end
 
 class String
   def hash
+    alphabets = ("a".."z").to_a
+    hash_num = 0
+    self.chars.each_with_index{|letter, idx| hash_num += alphabets.find_index(letter.downcase)*idx}
+    hash_num.hash
   end
 end
 
@@ -16,6 +23,8 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    hash_num = 0
+    self.keys.each{|key| hash_num += key.to_s.hash*self[key].hash}
+    hash_num
   end
 end
