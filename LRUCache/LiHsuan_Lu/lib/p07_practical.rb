e9d87bd771ef04035df33521f2c41f90d16ce8ea
travelingpiano <lihsuanlu@gmail.com>
2017-08-15 19:32:45 -0700

    fix palindrome bug of having letter count greater than 2

--- LRUCache/LiHsuan_Lu/lib/p07_practical.rb
+++ LRUCache/LiHsuan_Lu/lib/p07_practical.rb
@@ -7,8 +7,20 @@ def can_string_be_palindrome?(string)
   string.chars.each do |letter|
     if !hash_map.include?(letter)
       count += 1
-      hash_map.set(letter, letter)
+      hash_map.set(letter, 1)
+    else
+      #update number of letters
+      hash_map.set(letter,hash_map[letter]+1)
     end
   end
-  count-1 <= string.chars.length/2
+  odd = 0
+  hash_map.each do |key,val|
+    if val%2 == 1
+      odd += 1
+    end
+    if odd == 2
+      return false
+    end
+  end
+  true
 end
