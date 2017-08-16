a32091cb34011ffdb6dca2d78dc07a962786c13f
travelingpiano <lihsuanlu@gmail.com>
2017-08-16 11:40:18 -0700

    refactored palindrome string code

--- LRUCache/LiHsuan_Lu/lib/p07_practical.rb
+++ LRUCache/LiHsuan_Lu/lib/p07_practical.rb
@@ -2,11 +2,9 @@ require_relative 'p05_hash_map'
 
 def can_string_be_palindrome?(string)
   hash_map = HashMap.new(26)
-  count = 0
 
   string.chars.each do |letter|
     if !hash_map.include?(letter)
-      count += 1
       hash_map.set(letter, 1)
     else
       #update number of letters
