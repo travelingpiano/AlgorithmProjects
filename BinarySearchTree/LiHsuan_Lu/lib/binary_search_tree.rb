97ca59799a9f3b6af6da1f8d1a1f7362431817f0
travelingpiano <lihsuanlu@gmail.com>
2017-08-18 22:27:23 -0700

    finish binary search tree project

--- BinarySearchTree/LiHsuan_Lu/lib/binary_search_tree.rb
+++ BinarySearchTree/LiHsuan_Lu/lib/binary_search_tree.rb
@@ -44,41 +44,72 @@ class BinarySearchTree
   def delete(value)
     cur_node = find(value)
     #set root to nil if target is root
+    cur_node
     if cur_node == @root
       @root = nil
     else
       #if target node has no children
       if cur_node.left == nil && cur_node.right == nil
-        #find out which side the node belonged to
-        if cur_node.value > cur_node.parent.value
-          cur_node.parent.right = nil
-        else
-          cur_node.parent.left = nil
-        end
+        no_child_delete(cur_node)
       #if target node has one child
       elsif (cur_node.left==nil) ^ (cur_node.right==nil)
-        parent = cur_node.parent
-        if cur_node.left==nil
-          cur_node = cur_node.right
-        else
-          cur_node = cur_node.left
-        end
-        cur_node.parent = parent
+        one_child_delete(cur_node)
+      #if target has two children
+      else
+        two_children_delete(cur_node)
       end
     end
   end
 
   # helper method for #delete:
   def maximum(tree_node = @root)
+    max_node = tree_node
+    while max_node.right != nil
+      max_node = max_node.right
+    end
+    max_node
   end
 
   def depth(tree_node = @root)
+    #base case: return 1 if there are no children for the tree node
+    return 0 if tree_node.left == nil && tree_node.right == nil
+    if tree_node.left == nil
+      return 1 + depth(tree_node.right)
+    elsif tree_node.right == nil
+      return 1 + depth(tree_node.left)
+    else
+      if depth(tree_node.left) > depth(tree_node.right)
+        return 1 + depth(tree_node.left)
+      else
+        return 1 + depth(tree_node.right)
+      end
+    end
   end
 
   def is_balanced?(tree_node = @root)
+    min_max = [depth(tree_node.left),depth(tree_node.right)].minmax
+    min_max[1] <= min_max[0]
   end
 
   def in_order_traversal(tree_node = @root, arr = [])
+    #base case if no children
+    if tree_node.left == nil && tree_node.right == nil
+      arr.push(tree_node.value)
+      return arr
+    end
+    if tree_node.left == nil
+      arr.push(tree_node.value)
+      return in_order_traversal(tree_node.right,arr)
+    elsif tree_node.right == nil
+      arr = in_order_traversal(tree_node.left,arr)
+      arr.push(tree_node.value)
+      return arr
+    else
+      arr = in_order_traversal(tree_node.left,arr)
+      arr.push(tree_node.value)
+      arr = in_order_traversal(tree_node.right,arr)
+      return arr
+    end
   end
 
 
@@ -116,5 +147,61 @@ class BinarySearchTree
       cur_node = cur_node.left
     end
     cur_node
+  end
+
+  def no_child_delete(cur_node)
+    #find out which side the node belonged to
+    if cur_node.value > cur_node.parent.value
+      cur_node.parent.right = nil
+    else
+      cur_node.parent.left = nil
+    end
+  end
+
+  def one_child_delete(cur_node)
+    parent = cur_node.parent
+    #see if the new node was on left or right of previous node
+    if cur_node.left==nil
+      cur_node = cur_node.right
+    else
+      cur_node = cur_node.left
+    end
+    #see if the node was on the left or right of parent
+    cur_node.parent = parent
+    if cur_node.value < parent.value
+      cur_node.parent.left = cur_node
+    else
+      cur_node.parent.right = cur_node
+    end
+  end
+
+  def two_children_delete(cur_node)
+    #find max of left tree
+    new_node =  maximum(cur_node.left)
+    #check if new node has left child (certainly no right child by definition) and do appropriate replacement
+    unless new_node.left == nil
+      new_node.left.parent = new_node.parent
+      new_node.parent.right = new_node.left
+    else
+      new_node.parent.right = nil
+    end
+    #replace current node, first by changing pointer of the parent
+    if cur_node.value < cur_node.parent.value
+      cur_node.parent.left = new_node
+    else
+      cur_node.parent.right = new_node
+    end
+    #then by changing pointer of children
+    new_node.left = cur_node.left
+    new_node.right = cur_node.right
+    cur_node.left.parent = new_node
+    cur_node.right.parent = new_node
+    #for left, needs to check if the tree only had one element
+    if cur_node.left == new_node
+      new_node.left = nil
+    else
+      new_node.left = cur_node.left
+      cur_node.left.parent = new_node
+    end
   end
 end
--- BinarySearchTree/LiHsuan_Lu/lib/bst_practical.rb
+++ BinarySearchTree/LiHsuan_Lu/lib/bst_practical.rb
@@ -1,3 +1,24 @@
-
+require 'binary_search_tree'
 def kth_largest(tree_node, k)
+  checked = {}
+  count = 0
+  cur_node = tree_node
+  while count < k
+    cur_node = tree_node
+    flag = 0
+    until flag == 1
+      #we know we've reached the current maximum if the all right nodes are either missing or have been checked and we have not been checked yet
+      if (cur_node.right == nil || checked[cur_node.right.value]) && !checked[cur_node.value]
+        flag = 1
+      #if current node has been checked, all nodes right of it will have been checked also
+      elsif checked[cur_node.value]
+        cur_node = cur_node.left
+      else
+        cur_node = cur_node.right
+      end
+    end
+    checked[cur_node.value] = true
+    count += 1
+  end
+  return cur_node
 end
