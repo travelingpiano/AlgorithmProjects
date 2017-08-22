894563964d3f5bfa985d04dc70778fd42f64900f
travelingpiano <lihsuanlu@gmail.com>
2017-08-21 22:46:31 -0700

    finished graph and topo sort project

--- Graphs/LiHsuan_Lu/lib/install_order.rb
+++ Graphs/LiHsuan_Lu/lib/install_order.rb
@@ -10,5 +10,41 @@
 
 
 def install_order(arr)
-
+  #search for nodes with no dependencies
+  #first, search for max_id
+  max = arr.flatten.max
+  solutions = []
+  (1..max).to_a.each do |num|
+    solutions.push(num) unless arr.flatten.include?(num)
+  end
+  #make the graph
+  found = []
+  vertices_arr = []
+  arr.each do |dependency|
+    new_el = []
+    if found.find_index{|el| el.value == dependency[0]}
+      new_el.push(found[found.find_index{|el| el.value == dependency[0]}])
+    else
+      new_vertex = Vertex.new(dependency[0])
+      found.push(new_vertex)
+      new_el.push(new_vertex)
+    end
+    if found.find_index{|el| el.value == dependency[1]}
+      new_el.push(found[found.find_index{|el| el.value == dependency[1]}])
+    else
+      new_vertex = Vertex.new(dependency[1])
+      found.push(new_vertex)
+      new_el.push(new_vertex)
+    end
+    vertices_arr.push(new_el)
+  end
+  vertices_arr.each do |vertices|
+    new_edge = Edge.new(vertices[1],vertices[0])
+  end
+  ans = topological_sort(found)
+  sol = []
+  ans.each do |vertex|
+    sol << vertex.value
+  end
+  sol.concat(solutions)
 end
--- Graphs/LiHsuan_Lu/lib/topological_sort.rb
+++ Graphs/LiHsuan_Lu/lib/topological_sort.rb
@@ -6,6 +6,25 @@ def topological_sort(vertices)
   unless connected?(vertices) && !cyclic?(vertices)
     return []
   end
+  sorted = []
+  top = Queue.new
+  vertices.each do |vertex|
+    if vertex.in_edges.empty?
+      top.enq(vertex)
+    end
+  end
+  until top.empty?
+    current = top.pop
+    sorted << current
+    current.out_edges.each do |edge|
+      if edge.to_vertex.in_edges[0] == edge && edge.to_vertex.in_edges.length == 1
+        top.enq(edge.to_vertex)
+      else
+        edge.to_vertex.delete_in_edge(edge)
+      end
+    end
+  end
+  sorted
 end
 
 def connected?(vertices)
@@ -20,7 +39,6 @@ def connected?(vertices)
       cur_idx += 1
     end
   end
-
   #do bfs and see if we reach entire set
   found = [vertices[start_idx]]
   vertices[start_idx].out_edges.each do |edge|
@@ -42,6 +60,11 @@ def connected?(vertices)
         found.push(edge.to_vertex)
       end
     end
+    found[start_idx].in_edges.each do |edge|
+      unless found.include?(edge.from_vertex)
+        found.push(edge.from_vertex)
+      end
+    end
     start_idx += 1
   end
   false
@@ -55,8 +78,18 @@ def cyclic?(vertices)
       unless found.include?(edge.to_vertex)
       found.push(edge.to_vertex)
       end
-
     end
-
+    start_idx = 0
+    while start_idx < found.length
+      found[start_idx].out_edges.each do |edge|
+        if vertex == edge.to_vertex
+          return true
+        elsif !found.include?(edge.to_vertex)
+          found.push(edge.to_vertex)
+        end
+      end
+      start_idx += 1
+    end
   end
+  false
 end
